25cfc8637cd676c5805cc3a8a238bb61891b87cf
Persolute <1538520381@qq.com>
2025-03-03 16:17:40 +0800

    路由改成懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,58 +1,57 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+import Login from '@/views/login/index.vue'
+import StudentManagement from "@/views/admin/studentManagement/index.vue";
+import StudentAdminManagement from "@/views/admin/studentAdminManagement/index.vue";
+import StudentStatusRecord from "@/views/admin/studentStatusRecord/index.vue";
+import StudentStatusRecordDetail from "@/views/admin/studentStatusRecordDetail/index.vue";
+import StudentStatusRecordDate from "@/views/admin/studentStatusRecordDate/index.vue";
+import InternshipApplicationManagement from "@/views/admin/internshipApplicationManagement/index.vue";
+import TeamMemberManagement from "@/views/studentAdmin/teamMemberManagement/index.vue";
+import StudentLogin from '@/views/student/login/index.vue';
+import StudentHome from '@/views/student/home/index.vue';
+import StudentInternshipApplication from '@/views/student/internshipApplication/index.vue'
+import StudentQuestionnaire from '@/views/student/questionnaire/index.vue'
+
 Vue.use(VueRouter)
 
 export const constantRoutes = [{path: '/', redirect: '/login', hidden: true},
 
     {
-        path: '/login', name: 'Login', component: () => import('@/views/login'),
+        path: '/login', name: 'Login', component: Login,
     },
 
     {
-        path: '/studentManagement',
-        name: 'StudentManagement',
-        component: () => import('@/views/admin/studentManagement')
+        path: '/studentManagement', name: 'StudentManagement', component: StudentManagement
     }, {
-        path: '/studentAdminManagement',
-        name: 'StudentAdminManagement',
-        component: () => import('@/views/admin/studentAdminManagement')
+        path: '/studentAdminManagement', name: 'StudentAdminManagement', component: StudentAdminManagement
     }, {
-        path: '/studentStatusRecord',
-        name: 'StudentStatusRecord',
-        component: () => import('@/views/admin/studentStatusRecord')
+        path: '/studentStatusRecord', name: 'StudentStatusRecord', component: StudentStatusRecord
     }, {
-        path: '/studentStatusRecordDetail',
-        name: 'StudentStatusRecordDetail',
-        component: () => import('@/views/admin/studentStatusRecordDetail')
+        path: '/studentStatusRecordDetail', name: 'StudentStatusRecordDetail', component: StudentStatusRecordDetail
     }, , {
-        path: '/studentStatusRecordDate',
-        name: 'StudentStatusRecordDate',
-        component: () => import('@/views/admin/studentStatusRecordDate')
+        path: '/studentStatusRecordDate', name: 'StudentStatusRecordDate', component: StudentStatusRecordDate
     }, {
         path: '/internshipApplicationManagement',
         name: 'InternshipApplicationManagement',
-        component: () => import('@/views/admin/internshipApplicationManagement')
+        component: InternshipApplicationManagement
     },
 
     {
-        path: '/teamMemberManagement',
-        name: 'TeamMemberManagement',
-        component: () => import('@/views/studentAdmin/teamMemberManagement')
+        path: '/teamMemberManagement', name: 'TeamMemberManagement', component: TeamMemberManagement
     },
 
     {
-        path: '/student/login', name: 'StudentLogin', component: () => import('@/views/student/login')
+        path: '/student/login', name: 'StudentLogin', component: StudentLogin
     }, {
-        path: '/student/home', name: 'StudentHome', component: () => import('@/views/student/home')
+        path: '/student/home', name: 'StudentHome', component: StudentHome
     }, {
         path: '/student/internshipApplication',
         name: 'StudentInternshipApplication',
-        component: () => import('@/views/student/internshipApplication')
+        component: StudentInternshipApplication
     }, {
-        path: '/student/questionnaire',
-        name: 'StudentQuestionnaire',
-        component: () => import('@/views/student/questionnaire')
+        path: '/student/questionnaire', name: 'StudentQuestionnaire', component: StudentQuestionnaire
     }]
 
 const createRouter = () => new VueRouter({
